72bc3a2e1277332abc742e2cca2825338856ccc8
Ben Cipollini <bcipollini@classy.org>
2017-08-20 10:46:55 -0700

    RF: simplify the build process by passing the base url.

--- circle_urls.sh
+++
@@ -1,3 +0,0 @@
-BASEURL=https://${CIRCLE_BUILD_NUM}-${REPO_ID}-gh.circle-artifacts.com/0/home/ubuntu/nonprofit-open-data-collective.github.io/_site
-sed -i "14 s,.*,destination: ./_site,g" "_config.yml"
-sed -i "11 s,.*,baseurl: $BASEURL,g" "_config.yml"
