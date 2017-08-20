cb03315fdb23bfd39250d38c5023e9f46f550629
Ben Cipollini <bcipollini@classy.org>
2017-08-20 07:47:43 -0700

    BF: update circle build.

--- circle_urls.sh
+++ circle_urls.sh
@@ -1,4 +1,3 @@
-BASEURL=https://circle-artifacts.com/gh/Nonprofit-Open-Data-Collective
-/Nonprofit-Open-Data-Collective.github.io/$CIRCLE_BUILD_NUM/artifacts/0/home/ubuntu/Nonprofit-Open-Data-Collective.github.io/_site
+BASEURL=https://circle-artifacts.com/gh/bcipolli/Nonprofit-Open-Data-Collective.github.io/$CIRCLE_BUILD_NUM/artifacts/0/home/ubuntu/Nonprofit-Open-Data-Collective.github.io/_site
 sed -i "14 s,.*,destination: ./_site,g" "_config.yml"
 sed -i "11 s,.*,baseUrl: $BASEURL,g" "_config.yml"
